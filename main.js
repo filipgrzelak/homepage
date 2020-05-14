a539aaf6654011c05b2f830d934072ae4d9d9031
Filip Grzelak <filip_grzelak@wp.pl>
2020-05-14 13:09:58 +0200

    funny changes

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ const age = 19;
 console.log(firstName);
 console.log(age);
 console.log(`Nazywam się ${firstName} i mam ${age} lat.`);
+
+
+const heading = document.querySelector('.empty-box--js');
+
+heading.innerHTML = 'Jazda, jazda biała gwiazda!';
